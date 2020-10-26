9532d5174f9753ed234239bcce09a77311ab5390
Enindu Alahapperuma <enindu@gmail.com>
2020-10-26 23:39:41 +0530

    Add session middleware

---
+++ app/Middleware/Session.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Middleware;
+
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class Session
+{
+  private $settings;
+
+  /**
+   * Session constructor
+   * 
+   * @param array $settings
+   */
+  public function __construct(array $settings)
+  {
+    $this->settings = [
+      'name'      => isset($settings['name']) ? $settings['name'] : 'slender',
+      'lifetime'  => isset($settings['lifetime']) ? $settings['lifetime'] : 0,
+      'path'      => isset($settings['path']) ? $settings['path'] : '/',
+      'domain'    => isset($settings['domain']) ? $settings['domain'] : '',
+      'secure'    => isset($settings['secure']) ? $settings['secure'] : false,
+      'http-only' => isset($settings['http-only']) ? $settings['http-only'] : false
+    ];
+  }
+
+  /**
+   * Session invoker
+   * 
+   * @param Request                 $request
+   * @param RequestHandlerInterface $requestHandler
+   * 
+   * @return Response
+   */
+  public function __invoke(Request $request, RequestHandlerInterface $requestHandler): Response
+  {
+    $checkSessionCookie = isset($request->getCookieParams()[$this->settings['name']]);
+    if($checkSessionCookie) {
+      return $requestHandler->handle($request);
+    }
+
+    session_set_cookie_params([
+      'lifetime' => $this->settings['lifetime'],
+      'path'     => $this->settings['path'],
+      'domain'   => $this->settings['domain'],
+      'secure'   => $this->settings['secure'],
+      'httponly' => $this->settings['http-only']
+    ]);
+    session_name($this->settings['name']);
+    session_id(md5(uniqid(bin2hex(random_bytes(32)))));
+    session_start();
+
+    return $requestHandler->handle($request);
+  }
+}
--- index.php
+++ index.php
@@ -17,6 +17,7 @@ mb_internal_encoding($_ENV['APP_CHARSET']);
 
 // Containers
 $container = new Container();
+require_once __DIR__ . "/containers/session.php";
 require_once __DIR__ . "/containers/filesystem.php";
 require_once __DIR__ . "/containers/clock.php";
 require_once __DIR__ . "/containers/image.php";
