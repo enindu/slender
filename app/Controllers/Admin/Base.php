41c6561bf1492694a7c300e693d31a39fcbb0bf9
Enindu Alahapperuma <enindu@gmail.com>
2020-12-23 00:50:54 +0530

    Add admin homepage

---
+++ app/Controllers/Admin/Base.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\Controller;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class Base extends Controller
+{
+  /**
+   * Homepage
+   * 
+   * @param Request  $request
+   * @param Response $response
+   * @param array    $data
+   * 
+   * @return Response
+   */
+  public function home(Request $request, Response $response, array $data): Response
+  {
+    return $this->view($response, '@admin/home.twig');
+  }
+}
--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Controllers\Admin\Base as AdminBase;
 use App\Controllers\User\Base as UserBase;
+use App\Middleware\AdminAuth;
 use Slim\Routing\RouteCollectorProxy;
+
+$app->group('/admin', function(RouteCollectorProxy $admin) {
+  $admin->get('', AdminBase::class . ':home');
+})->add(new AdminAuth($container));
 
 // User routes
 $app->group('/', function(RouteCollectorProxy $user) {
