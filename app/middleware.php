e09c37bdebf41a8923486f1bf7a2c6d9cd4ae2f2
Enindu Alahapperuma <enindu@gmail.com>
2021-02-06 13:42:32 +0530

    Add error renderer

--- app/middleware.php
+++ app/middleware.php
@@ -1,5 +1,10 @@
 <?php
 
+use System\Slim\ErrorRenderer;
+
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, true, true);
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorHandler = $errorMiddleware->getDefaultErrorHandler();
+$errorHandler->registerErrorRenderer("text/html", new ErrorRenderer($container));
---
+++ resources/common/assets/js/script.js
@@ -0,0 +1,2 @@
+window.addEventListener("DOMContentLoaded",(function(){var n=document.getElementById("back");null!=n&&n.addEventListener("click",(function(){window.history.go(-1)}))}));
+//# sourceMappingURL=script.js.map
