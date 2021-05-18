1d1b4a02a28da5521833c7ca020ab00ebaa754fd
Enindu Alahapperuma <enindu@gmail.com>
2021-05-19 00:31:39 +0530

    Clean code

--- system/Slender/Password.php
+++ system/Slender/Password.php
@@ -4,14 +4,9 @@ namespace System\Slender;
 
 class Password
 {
-  public static function create(string $password, string $algorithm = PASSWORD_ARGON2ID): string|null
+  public static function create(string $password, string $algorithm = PASSWORD_ARGON2ID): string
   {
-    $hash = password_hash($password . $_ENV["app"]["key"], $algorithm);
-    if($hash == false) {
-      return null;
-    }
-
-    return $hash;
+    return password_hash($password . $_ENV["app"]["key"], $algorithm);
   }
 
   public static function verify(string $password, string $hash): bool
