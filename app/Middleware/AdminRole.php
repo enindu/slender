cde7f16222b4cfac85c819a6193fd9fb043925c2
Enindu Alahapperuma <enindu@gmail.com>
2021-05-18 16:58:46 +0530

    Clean AdminRole middleware class

--- app/Middleware/AdminRole.php
+++ app/Middleware/AdminRole.php
@@ -17,8 +17,7 @@ class AdminRole
     $eloquent = $this->container->get("eloquent");
 
     $roleID = $eloquent->table("admins")->where("unique_id", $request->getCookieParams()[$_ENV["app"]["cookie"]["admin"]])->value("role_id");
-    $roleExists = array_search($roleID, $this->roles);
-    if($roleExists === false) {
+    if(array_search($roleID, $this->roles) === false) {
       throw new HttpForbiddenException($request);
     }
 
