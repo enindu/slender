ef2f1848620c88dc1cbe84ba1fff6760c106be7c
Enindu Alahapperuma <enindu@gmail.com>
2021-05-22 13:19:06 +0530

    Fix session cookie issues

--- settings/system.php
+++ settings/system.php
@@ -3,11 +3,11 @@
 date_default_timezone_set($_ENV["app"]["timezone"]);
 mb_internal_encoding($_ENV["app"]["charset"]);
 session_start([
-  "name"            => $_ENV["middleware"]["session"]["name"],
-  "cookie_lifetime" => $_ENV["middleware"]["session"]["lifetime"],
-  "cookie_path"     => $_ENV["middleware"]["session"]["path"],
-  "cookie_domain"   => $_ENV["middleware"]["session"]["domain"],
-  "cookie_secure"   => $_ENV["middleware"]["session"]["secure"],
-  "cookie_httponly" => $_ENV["middleware"]["session"]["http-only"],
-  "sid_length"      => $_ENV["middleware"]["session"]["sid-length"]
+  "name"            => $_ENV["session"]["name"],
+  "cookie_lifetime" => $_ENV["session"]["lifetime"],
+  "cookie_path"     => $_ENV["session"]["path"],
+  "cookie_domain"   => $_ENV["session"]["domain"],
+  "cookie_secure"   => $_ENV["session"]["secure"],
+  "cookie_httponly" => $_ENV["session"]["http-only"],
+  "sid_length"      => $_ENV["session"]["sid-length"]
 ]);
