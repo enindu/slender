471ba5cb11854a18e4289a94c9dda3755e40a05d
Enindu Alahapperuma <enindu@gmail.com>
2021-09-07 16:47:32 +0530

    Add API method middleware

---
+++ app/Middleware/Api/Method.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Middleware\Api;
+
+use DI\Container;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use System\App\Middleware;
+
+class Method extends Middleware
+{
+    public function __construct(private Container $container, private string $method)
+    {
+        parent::__construct($container);
+    }
+
+    public function __invoke(Request $request, RequestHandlerInterface $requestHandler): Response
+    {
+        $method = $request->getMethod();
+        if($method != $this->method) {
+            return $this->newJsonResponse([
+                "status"  => false,
+                "message" => "Method not allowed."
+            ], 405);
+        }
+
+        return $requestHandler->handle($request);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Controllers\Api\Base;
 use App\Middleware\Api\Authentication;
+use App\Middleware\Api\Method;
 use Slim\Routing\RouteCollectorProxy;
 
 $authentication = new Authentication($container);
+$putMethod = new Method($container, "PUT");
 
 $app->group("/api", function(RouteCollectorProxy $api): void {
     $api->any("", Base::class . ":base");
-})->add($authentication);
+})->add($authentication)->add($putMethod);
