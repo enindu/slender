166067b86ae836a7b5a7aa6e75dfb7365bf7095d
Enindu Alahapperuma <enindu@gmail.com>
2021-09-23 17:55:20 +0530

    Fix globals typo

--- system/View/Globals.php
+++ system/View/Globals.php
@@ -14,12 +14,7 @@ class Globals extends AbstractExtension implements GlobalsInterface
     {
         return [
             "app"      => $_ENV["app"],
-            "settings" => $_ENV["settings"],
-            "auth"     => [
-                "exists" => isset($_SESSION["account"]),
-                "id"     => $_SESSION["account"]["id"] ?? false,
-                "role"   => $_SESSION["account"]["role"] ?? false
-            ]
+            "settings" => $_ENV["settings"]
         ];
     }
 }
