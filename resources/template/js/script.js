f035fa688d4b64a1c0405a8cf785f57d3b53a48e
Enindu Alahapperuma <enindu@gmail.com>
2021-10-01 02:10:17 +0530

    Fix Parcel import issues

--- resources/template/js/script.js
+++ resources/template/js/script.js
@@ -1,6 +1,8 @@
-import "halfmoon";
+import halfmoon from "~/node_modules/halfmoon/js/halfmoon-module.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+    halfmoon.onDOMContentLoaded();
+
     const back = document.getElementById("back");
     if(back != null) {
         back.addEventListener("click", (event) => {
